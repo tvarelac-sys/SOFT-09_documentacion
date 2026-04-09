1b4d09f9a85fe7b191eca0df1a3f3ebe746bf3ab
Tati Varela <tatvarela@MacBook-Air-de-Tati.local>
2026-04-09 17:50:06 -0400

    Actualización del saludo

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,7 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, mundo desde Java!");
-        saludar("Tatiana");
+        saludar("Hola Profe Stephanie");
         sumar(5, 3);
     }
 
